1d59a2bc1300469a224cc8d3890ad425680622df
Tom Paul <thomasp@stratusadv.com>
2023-02-20 20:49:15 -0700

    Initial Setup of TPP

--- app/system/base_settings.py
+++ app/system/base_settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'app.wsgi.application'
+WSGI_APPLICATION = 'app.system.wsgi.application'
 
 
 # Database
